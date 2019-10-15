d3d8c74eb549af45dc925e64a9b8efe128e8b20e
EEEeelijaHhHHh <elijahs.adjutovs@gmail.com>
2019-10-15 20:48:13 +0300

    Added layouts and imported template to vue

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,19 +1,4 @@
-<!doctype html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport"
-        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-  <link rel="stylesheet" href="index.css">
-  <title>Document</title>
-</head>
-
-
-<body>
-<div>
+<template>
   <div class="app-main-layout">
     <nav class="navbar orange lighten-1">
       <div class="nav-wrapper">
@@ -26,16 +11,12 @@
 
         <ul class="right hide-on-small-and-down">
           <li>
-            <a
-                class="dropdown-trigger black-text"
-                href="#"
-                data-target="dropdown"
-            >
+            <a class="dropdown-trigger black-text" href="#" data-target="dropdown">
               USER NAME
               <i class="material-icons right">arrow_drop_down</i>
             </a>
 
-            <ul id='dropdown' class='dropdown-content'>
+            <ul id="dropdown" class="dropdown-content">
               <li>
                 <a href="#" class="black-text">
                   <i class="material-icons">account_circle</i>Профиль
@@ -71,44 +52,12 @@
       </li>
     </ul>
 
-    <main class="app-content">
-      <div class="app-page">
+    <router-view />
 
-<div>
-  <div class="page-title">
-    <h3>Профиль</h3>
-  </div>
-
-  <form class="form">
-    <div class="input-field">
-      <input
-          id="description"
-          type="text"
-      >
-      <label for="description">Имя</label>
-      <span
-            class="helper-text invalid">name</span>
+    <div class="fixed-action-btn">
+      <a class="btn-floating btn-large blue" href="#">
+        <i class="large material-icons">add</i>
+      </a>
     </div>
-
-    <button class="btn waves-effect waves-light" type="submit">
-      Обновить
-      <i class="material-icons right">send</i>
-    </button>
-  </form>
-</div>
-
-</div>
-</main>
-
-<div class="fixed-action-btn">
-  <a class="btn-floating btn-large blue" href="#">
-    <i class="large material-icons">add</i>
-  </a>
-</div>
-</div>
-</div>
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-</body>
-</html>
-
+  </div>
+</template>
--- src/router.js
+++ src/router.js
@@ -2,22 +2,27 @@ import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
 
+
 Vue.use(Router);
 
 export default new Router({
   mode: "history",
   base: process.env.BASE_URL,
-  routes: [
-    {
+  routes: [{
       path: "/",
       name: "home",
+      meta: {
+        layout: "main"
+      },
       component: Home
     },
     {
-      path: "/about",
-      name: "about",
-      component: () =>
-        import("./views/About.vue")
+      path: "/login",
+      name: "login",
+      meta: {
+        layout: "empty"
+      },
+      component: () => import("./views/Login.vue")
     }
   ]
 });
