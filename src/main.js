dd14e4691a432b23dbd2baa852365c3553d27157
EEEeelijaHhHHh <elijahs.adjutovs@gmail.com>
2019-10-17 11:53:32 +0300

    Added Loader to the Home page

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import router from "./router";
 import store from "./store";
 import dateFilter from "@/filters/date.filter";
 import messagePlugin from "@/utils/message.plugin";
+import Loader from "@/components/app/Loader";
 import "./registerServiceWorker";
 import "materialize-css/dist/js/materialize.min";
 
@@ -18,6 +19,8 @@ Vue.use(Vuelidate);
 Vue.use(messagePlugin);
 
 Vue.filter("date", dateFilter);
+
+Vue.component("Loader", Loader);
 
 firebase.initializeApp({
   apiKey: "AIzaSyANiew2FtjFbiQioria3iQrxWbIn4_EwMQ",
