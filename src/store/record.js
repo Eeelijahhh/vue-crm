f806b242fabfec86770581cd9d2248c2801e20f8
EEEeelijaHhHHh <elijahs.adjutovs@gmail.com>
2019-10-18 16:41:22 +0300

    Logic for the Planning & new action fetchRecords & categories transform in the Planning

--- src/store/record.js
+++ src/store/record.js
@@ -10,6 +10,17 @@ export default {
         commit("setError", error);
         throw error;
       }
+    },
+        async fetchRecords({ commit, dispatch }) {
+      try {
+        const userId = await dispatch("getUserId");
+        const records = (await firebase.database().ref(`/users/${userId}/records`).once("value")).val() || {};
+        // tranform records array
+        return Object.keys(records).map(key => ({ ...records[key], id: key }))
+      } catch (error) {
+        commit("setError", error);
+        throw error;
+      }
     }
   }
 }
