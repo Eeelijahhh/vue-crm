990319f6f91e3cc888795ce419b584a1854ff7bf
EEEeelijaHhHHh <elijahs.adjutovs@gmail.com>
2019-10-20 23:43:43 +0300

    Changed minimum fraction digits in currency filter to 0

--- src/filters/currency.filter.js
+++ src/filters/currency.filter.js
@@ -1,6 +1,7 @@
 export default function currencyFilter(value, currency = "EUR") {
   return Intl.NumberFormat("ru-RU", {
     style: "currency",
+    minimumFractionDigits: 0,
     currency
   }).format(value)
 }
