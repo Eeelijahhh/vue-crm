1c35a20fb06c69021478f3c8338d579e35cd2612
EEEeelijaHhHHh <elijahs.adjutovs@gmail.com>
2019-10-22 19:02:56 +0300

    Change name in the Profile

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -4,11 +4,15 @@
       <h3>Профиль</h3>
     </div>
 
-    <form class="form">
+    <form class="form" @submit.prevent="submitHandler">
       <div class="input-field">
-        <input id="description" type="text" />
+        <input id="description" type="text" v-model="name" />
         <label for="description">Имя</label>
-        <span class="helper-text invalid">name</span>
+        <small v-if="$v.name.$dirty && !$v.name.required" class="helper-text invalid">Введите имя</small>
+        <small
+          v-else-if="$v.name.$dirty && !$v.name.required"
+          class="helper-text invalid"
+        >Имя должен быть больше {{ this.$v.name.$params.minLength.min }} символов</small>
       </div>
 
       <button class="btn waves-effect waves-light" type="submit">
@@ -18,3 +22,41 @@
     </form>
   </div>
 </template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+import { required, minLength } from 'vuelidate/lib/validators'
+
+export default {
+  name: 'profile',
+  data: () => ({
+    name: ''
+  }),
+  computed: {
+    ...mapGetters(['info'])
+  },
+  validations: {
+    name: { required, minLength: minLength(3) }
+  },
+  methods: {
+    ...mapActions(['updateInfo']),
+    async submitHandler() {
+      if (this.$v.$invalid) {
+        this.$v.$touch()
+        return
+      }
+      try {
+        this.updateInfo({
+          name: this.name
+        })
+      } catch (error) {}
+    }
+  },
+  mounted() {
+    this.name = this.info.name
+    setTimeout(() => {
+      M.updateTextFields()
+    })
+  }
+}
+</script>
