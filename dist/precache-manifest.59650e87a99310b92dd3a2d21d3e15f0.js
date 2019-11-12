bffe9e9b28ef75beefb499f4cb14f2f192360a02
EEEeelijaHhHHh <elijahs.adjutovs@gmail.com>
2019-11-12 13:08:01 +0200

    Added server & heroku

---
+++ dist/precache-manifest.59650e87a99310b92dd3a2d21d3e15f0.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "428d6bfd18d8f147c633",
+    "url": "/js/chunk-5f4ad635.f0817237.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "c626aff43dc6ea92c24a",
+    "url": "/css/chunk-7be10dc8.fbb7b0b0.css"
+  },
+  {
+    "revision": "9544fe6703837f931d31",
+    "url": "/js/chunk-2d0d05dc.26b2253d.js"
+  },
+  {
+    "revision": "f01835bd1f49eb34c682",
+    "url": "/js/chunk-2d21a3d2.570fb91c.js"
+  },
+  {
+    "revision": "9bb24c6b3f8abda5a77b",
+    "url": "/js/chunk-4e668e14.1bede760.js"
+  },
+  {
+    "revision": "f7c1404bb3a9612443c8",
+    "url": "/js/chunk-vendors.80fac806.js"
+  },
+  {
+    "revision": "85d44a4ffb95bf789812",
+    "url": "/js/chunk-5831204f.627143fa.js"
+  },
+  {
+    "revision": "2a33003a0cb83124f295",
+    "url": "/js/chunk-c420df12.ebc404ed.js"
+  },
+  {
+    "revision": "e4cdb227c0de7c5f6f51",
+    "url": "/js/chunk-2d217a99.90a2d1e4.js"
+  },
+  {
+    "revision": "c626aff43dc6ea92c24a",
+    "url": "/js/chunk-7be10dc8.edea8c25.js"
+  },
+  {
+    "revision": "69d90975e9d917f23a09",
+    "url": "/js/chunk-9d1675fc.9cf8f26f.js"
+  },
+  {
+    "revision": "7e3fde145432b6044ce5",
+    "url": "/js/app.374630cb.js"
+  },
+  {
+    "revision": "71d44bb097bff705bb7c83c0c428dcae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7c1404bb3a9612443c8",
+    "url": "/css/chunk-vendors.5abecdc7.css"
+  },
+  {
+    "revision": "85d44a4ffb95bf789812",
+    "url": "/css/chunk-5831204f.c2507a61.css"
+  },
+  {
+    "revision": "7e3fde145432b6044ce5",
+    "url": "/css/app.4e8e286f.css"
+  }
+];
